f33ca0b3cd8c0e5a3e990f557ccc1d02782a8f93
Tristan <29161072+trisbman@users.noreply.github.com>
2023-09-06 11:25:26 +0700

    fix submission

---
+++ script.js
@@ -0,0 +1,44 @@
+window.onload = () => handler();
+window.onscroll = () => sticky();
+
+const toggleBtn = document.getElementById("toggle");
+const menuListContainer = document.getElementById("menu");
+const topNavChild = document.getElementById("navbar-div");
+const topNav = topNavChild.parentElement;
+const container = topNav.parentElement;
+
+toggleBtn.addEventListener("click", toggle);
+
+const container__height = getComputedStyle(container).height;
+const offsetTop = parseInt(topNav.offsetTop);
+
+function handler() {
+  if (getComputedStyle(toggleBtn).display === "block") {
+    menuListContainer.style.height = container__height;
+    topNav.children[0].lastElementChild.removeChild(menuListContainer);
+  }
+}
+
+function toggle() {
+  if (getComputedStyle(menuListContainer)[0])
+    topNavChild.removeChild(menuListContainer);
+  else {
+    menuListContainer.style.visibility = "visible";
+    topNavChild.appendChild(menuListContainer, container.firstElementChild);
+  }
+}
+
+function sticky() {
+  if (window.scrollY >= offsetTop) {
+    topNav.classList.add("sticky");
+    topNav.classList.add("content");
+  } else {
+    topNav.classList.remove("sticky");
+  }
+
+  document
+    .querySelector(".profile-img")
+    .addEventListener("click", () =>
+      window.open("https://www.dicoding.com/users/tristanb")
+    );
+}
